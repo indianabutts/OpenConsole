e63f1685c076e502522bba7ae70334057d71e055
Yassir Al-Refaie <yassir.alrefaie@gmail.com>
2013-08-11 23:56:57 +0100

    added program counter code

--- source/systemverilog/gpu/main/gpu_pc.sv
+++ source/systemverilog/gpu/main/gpu_pc.sv
@@ -1,5 +1,18 @@
 module gpu_pc(
+	      input clk,
+	      input reset,
+	      input [31:0] input_pc,
+	      output [31:0] output_pc
 	      );
+
+   always_ff@(posedge clk or posedge reset)
+     begin
+	if (reset)
+	  output_pc <= 0;
+	else
+	  output_pc<=input_pc;
+	
+     end
 
 
 endmodule // gpu_pc
